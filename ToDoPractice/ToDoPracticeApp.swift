75a3be6a2edd6953f29dac41714f5c9ce0e3ec84
Kiara Eunice Abamonga <kiara.abamonga@gmail.com>
2023-06-28 10:47:55 -0400

    Initial Commit

---
+++ ToDoPractice/ToDoPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoPracticeApp.swift
+//  ToDoPractice
+//
+//  Created by Kiara on 6/28/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
